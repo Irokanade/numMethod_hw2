db0c3a407ffe13f433c2f587e591e128f8bcc5a1
irokanade <michaeljs.leong@gmail.com>
2022-09-23 16:48:42 +0800

    init commit

--- numMethod_hw2/main.c
+++ numMethod_hw2/main.c
@@ -6,9 +6,24 @@
 //
 
 #include <stdio.h>
+#include <math.h>
+
+double g(double x) {
+    return cbrt(2*x+2);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    double oldX = 0.5;
+    double newX = 0.5;
+    
+    do {
+        double temp = newX;
+        newX = g(newX);
+        oldX = temp;
+        //printf("%lf\n", newX);
+    } while(!(fabs(newX-oldX) < 0.000001));
+    
+    printf("%lf\n", newX);
+    
     return 0;
 }
